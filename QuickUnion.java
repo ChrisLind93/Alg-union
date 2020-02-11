664408c2aaede6c1a727483cdb8e536f103ffac1
chrislind93 <lolsovsen@hotmail.com>
2020-02-11 22:07:35 +0100

    test

---
+++ QuickUnion.java
@@ -0,0 +1,26 @@
+public class QuickUnion {
+    private int[] id;
+    public static void main(String[] args) {
+        QuickUnion qu = new QuickUnion(10);
+        qu.unite(p, q);
+    }
+
+    public QuickUnion(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) id[i] = i;
+    }
+
+    private int root(int i) {
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+    public boolean find(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void unite(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
